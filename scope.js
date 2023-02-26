af4bfc365c38181076520f7c694ff7e7065d164a
tuhin_4ever <tuhin4ever19@gmail.com>
2023-02-27 01:18:04 +0600

    second commit

---
+++ scope.js
@@ -0,0 +1,31 @@
+// global scope
+// local scope
+// block scope
+
+// global scope
+// let Name = 'captain'
+// let isMale = true;
+
+{ // block scope{ inside this bracket}
+    // let roll = 12
+    // console.log(roll)
+
+    // var roll = 12
+    
+}
+// console.log(roll)
+
+// local scope
+// function add() {
+//   const  a = 5;
+// }
+// console.log(a)
+
+
+let Name = 'captain'
+const isMale = true;
+
+{
+    let Name = 'captain'
+    console.log(Name)
+}
---
+++ template.js
@@ -0,0 +1,13 @@
+const Name = 'mehedy';
+const age = 43;
+const job = 'bekar';
+
+// const sentence = 'Amar naam '+ Name + '\n Amar boyos '+ age + '\n ami '+job;
+
+const sentence = `
+    Amar Naam ${Name}
+    amar boyos ${age}
+    ami ${job}
+`
+
+console.log(sentence)
